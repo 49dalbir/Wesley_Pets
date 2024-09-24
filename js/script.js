048c372ba3fa102e95bd4ccc755ddea720574b04
Dalbir Singh <157403229+49dalbir@users.noreply.github.com>
2024-09-24 19:16:30 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,3 +23,74 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('Item added to cart');
     });
 });
+
+// HOVER EFFECTS
+const shopFigures = document.querySelectorAll('#section4 figure');
+
+shopFigures.forEach(figure => {
+    const image = figure.querySelector('img');
+    const figcaption = figure.querySelector('figcaption');
+
+    figure.addEventListener('mouseover', () => {
+        image.style.transform = 'scale(1.1)';
+        figcaption.style.transform = 'scale(1.1)';
+        image.style.transition = 'transform 0.3s';
+        figcaption.style.transition = 'transform 0.3s';
+    });
+
+    figure.addEventListener('mouseout', () => {
+        image.style.transform = 'scale(1)';
+        figcaption.style.transform = 'scale(1)';
+    });
+});
+
+const navbarItems = document.querySelectorAll('.navbar li');
+
+navbarItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.transform = 'scale(1.1)';
+        item.style.transition = 'transform 0.3s';
+    });
+
+    item.addEventListener('mouseout', () => {
+        item.style.transform = 'scale(1)';
+    });
+});
+
+
+const figures = document.querySelectorAll('#shop_section figure');
+
+figures.forEach(figure => {
+    const image = figure.querySelector('img');
+    const figcaption = figure.querySelector('figcaption');
+
+    figure.addEventListener('mouseover', () => {
+        image.style.transform = 'scale(1.1)';
+        figcaption.style.transform = 'scale(1.1)';
+        image.style.transition = 'transform 0.3s';
+        figcaption.style.transition = 'transform 0.3s';
+    });
+
+    figure.addEventListener('mouseout', () => {
+        image.style.transform = 'scale(1)';
+        figcaption.style.transform = 'scale(1)';
+    });
+});
+
+
+const giftcard = document.getElementById('giftcard');
+const giftcardImage = giftcard.querySelector('img');
+const figcaption = giftcard.querySelector('figcaption');
+
+giftcard.addEventListener('mouseover', () => {
+    giftcardImage.style.transform = 'scale(1.1)';
+    figcaption.style.transform = 'scale(1.1)';
+    giftcardImage.style.transition = 'transform 0.3s';
+    figcaption.style.transition = 'transform 0.3s';
+});
+
+giftcard.addEventListener('mouseout', () => {
+    giftcardImage.style.transform = 'scale(1)';
+    figcaption.style.transform = 'scale(1)';
+});
+
